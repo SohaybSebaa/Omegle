e0712c751d0cc8f8e5e89ccfbfb0dbf6a7730333
PersistentGuy <95829882+PersistentGuy@users.noreply.github.com>
2023-06-25 17:10:43 +0100

    One on One

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO, send, join_room, leave_room, emit
+from flask_socketio import SocketIO, send, join_room, leave_room, emit, rooms
 import random
 
 app = Flask(__name__)
@@ -10,27 +10,42 @@ names = ['Bou', 'Daa']
 # List to store waiting users
 waiting_users = []
 
-
+# ex
 @socketio.on('connect')
 def handle_connect():
 
     user_id = request.sid
-    
-    # Check if there is another waiting user
-    if waiting_users:
-        # Get the first waiting user
-        partner_id = waiting_users.pop()
-        room = f"room-{user_id}-{partner_id}"
-        
-        # Get the second waiting user
-        join_room(room, sid=user_id)
-        join_room(room, sid=partner_id)
+    print(f'User {user_id} connected')
+    print(f'User {user_id} assigned rooms: {rooms(sid=user_id)}')
 
-        send('You are now connected to a creep', room=room)
+    if not waiting_users:
+        waiting_users.append(user_id)
+        room_name = f"{user_id}"
+        join_room(room_name, sid=user_id)
+        print(f'User {user_id} joining the room {room_name}')
+        #join_room(f"room-{user_id}", sid=user_id)
+        send('Waiting for a user...', room=user_id)
+        print(f'User {user_id} created and joined the room {user_id}')
+        print(f'User {user_id} assigned rooms: {rooms(sid=user_id)}')
+        
+        user_rooms = rooms(sid=user_id)
+        if room_name in user_rooms:
+            print(f"User {user_id} is IN {room_name}")
+        else:
+            print(f"User {user_id} is NOT IN {room_name}")
         
     else:
-        waiting_users.append(user_id)
-        send('Waiting for a user...', room=user_id)
+        waiting_user = waiting_users.pop()
+        waiting_room = f"{waiting_user}"
+
+        join_room(waiting_room, sid=user_id)
+        join_room(waiting_room, sid=waiting_user)
+
+        send('You are now connected to a creep', room=waiting_room)
+        print(f'User {user_id} joined room {waiting_room}')
+        print(f'User {user_id} assigned rooms: {rooms(sid=user_id)}')
+#
+
 
 @app.route("/")
 def hello_world():
@@ -43,18 +58,32 @@ def peer_to_peer():
 # new
 @socketio.on('join')
 def on_join(data):
-    username = random.choice(names)
-    names.remove(username)
+
     room = request.sid
     join_room(room)
-    socketio.emit('assign_name', {'username': username}, room=room)
+    socketio.emit('assign_name', room=room)
 
 @socketio.on('send_message')
 def send_msg(data):
     message = data['message']
     username = request.sid
-    socketio.emit('chat', {'username': username, 'message': message})
+    # Get the room the user is currently in
+    user_rooms = rooms(sid=username)
+    # Get the room the user is currently in
+    room = [room for room in rooms(sid=username)][0]
+    socketio.emit('chat', {'username': username, 'message': message}, room=room)
 
+
+
+@socketio.on('message')
+def handle_message(msg):
+    user_id = request.sid
+    room_name = next((room for room in rooms(sid=user_id) if room.startswith("room-")), None)
+    print(f'User {user_id} attempted to send message "{msg}"')
+    if room_name is not None:
+        print(f'User {user_id} sent message "{msg}" in room {room_name}')
+    else:
+        print(f'User {user_id} not in room. Message not sent.')
 
 if __name__ == '__main__':
       socketio.run(app)
